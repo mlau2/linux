d4edccf9bc97a7ebda407e39b7bef8cc7946c863
Martin KaFai Lau <kafai@fb.com>
2015-02-06 13:53:07 -0800

    ipv6: Remove rt6_set_from() and its dst.from usage
    This patch removes the dst.from chain which is used to track the
    the expiration of the original route.  A ref-counted struct rt6_shared_info is
    used to replace it.
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>

--- include/net/ip6_fib.h
+++ include/net/ip6_fib.h
@@ -91,6 +91,11 @@ struct rt6key {
 
 struct fib6_table;
 
+struct rt6i_shared_info {
+	unsigned long	expires;
+	atomic_t	ref;
+};
+
 struct rt6_info {
 	struct dst_entry		dst;
 
@@ -113,6 +118,7 @@ struct rt6_info {
 	unsigned int			rt6i_nsiblings;
 
 	atomic_t			rt6i_ref;
+	struct rt6i_shared_info		*rt6i_shi;
 
 	/* These are in a separate cache line. */
 	struct rt6key			rt6i_dst ____cacheline_aligned_in_smp;
@@ -168,34 +174,16 @@ static inline void rt6_clean_expires(struct rt6_info *rt)
 {
 	rt->rt6i_flags &= ~RTF_EXPIRES;
 	rt->dst.expires = 0;
+	if (!(rt->rt6i_flags & RTF_CACHE))
+		rt->rt6i_shi->expires = 0;
 }
 
 static inline void rt6_set_expires(struct rt6_info *rt, unsigned long expires)
 {
 	rt->dst.expires = expires;
+	if (!(rt->rt6i_flags & RTF_CACHE))
+		rt->rt6i_shi->expires = expires;
 	rt->rt6i_flags |= RTF_EXPIRES;
-}
-
-static inline void rt6_update_expires(struct rt6_info *rt0, int timeout)
-{
-	struct rt6_info *rt;
-
-	for (rt = rt0; rt && !(rt->rt6i_flags & RTF_EXPIRES);
-	     rt = (struct rt6_info *)rt->dst.from);
-	if (rt && rt != rt0)
-		rt0->dst.expires = rt->dst.expires;
-
-	dst_set_expires(&rt0->dst, timeout);
-	rt0->rt6i_flags |= RTF_EXPIRES;
-}
-
-static inline void rt6_set_from(struct rt6_info *rt, struct rt6_info *from)
-{
-	struct dst_entry *new = (struct dst_entry *) from;
-
-	rt->rt6i_flags &= ~RTF_EXPIRES;
-	dst_hold(new);
-	rt->dst.from = new;
 }
 
 static inline void ip6_rt_put(struct rt6_info *rt)
